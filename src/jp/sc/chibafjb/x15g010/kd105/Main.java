3d9bed1cceedf1c1016e0cbc6990184b79893d30
unknown <x15g010@PC504N014.chiba-fjb.ac.jp>
2015-11-02 15:04:31 +0900

    課題5

---
+++ src/jp/sc/chibafjb/x15g010/kd105/Main.java
@@ -0,0 +1,12 @@
+package jp.sc.chibafjb.x15g010.kd105;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("氏名: 船橋  太郎(ふなばし  たろう)");
+		System.out.println("好きなエナジードリンク: M3");
+		System.out.println("嫌いなエナジードリンク: カオス");
+
+	}
+
+}
