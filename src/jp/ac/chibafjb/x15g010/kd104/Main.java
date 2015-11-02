00fcf3373c739cb3539c05ce79a84e4371082843
unknown <x15g010@PC504N014.chiba-fjb.ac.jp>
2015-11-02 15:05:10 +0900

    kadai5

--- src/jp/ac/chibafjb/x15g010/kd104/Main.java
+++ src/jp/ac/chibafjb/x15g010/kd104/Main.java
@@ -3,10 +3,15 @@ package jp.ac.chibafjb.x15g010.kd104;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("氏名: 船橋  太郎(ふなばし  たろう)");
-		System.out.println("好きなエナジードリンク: M3");
-		System.out.println("嫌いなエナジードリンク: カオス");
-
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.printf("あいうえお \n");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.printf("あいうえお \n");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
+		System.out.print("あいうえお ");
 	}
 
 }
