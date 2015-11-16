98ee630776b7f1bdd8632681da430bf1be2303ee
unknown <x15g010@PC504N014.chiba-fjb.ac.jp>
2015-11-16 14:55:36 +0900

    課題20

---
+++ src/jp/sc/chibafjb/x15g010/kdT023/Main.java
@@ -0,0 +1,13 @@
+package jp.sc.chibafjb.x15g010.kdT023;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=50;i<=70;i=i+5)
+
+			System.out.println(i);
+
+	}
+
+}
