83bfb93436bff5340b9b973d7339dbee6c977ca2
unknown <x15g010@PC504N014.chiba-fjb.ac.jp>
2015-12-07 14:16:57 +0900

    課題23

---
+++ src/jp/sc/chibafjb/x15g010/kd133/Main.java
@@ -0,0 +1,15 @@
+package jp.sc.chibafjb.x15g010.kd133;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		x = 5;
+		y = 10;
+		System.out.println("x+y="+ (x + y));
+				
+
+	}
+
+}
