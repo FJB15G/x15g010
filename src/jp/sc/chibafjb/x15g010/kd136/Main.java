fda5b2a9a254baa0e69f52181e79f5c05c4cae96
unknown <x15g010@PC504N014.chiba-fjb.ac.jp>
2015-12-14 14:29:18 +0900

    課題26

---
+++ src/jp/sc/chibafjb/x15g010/kd136/Main.java
@@ -0,0 +1,18 @@
+package jp.sc.chibafjb.x15g010.kd136;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 1;
+		int c = 0;
+		c = b;
+		b = a;
+		a = c;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+
+	}
+
+}
