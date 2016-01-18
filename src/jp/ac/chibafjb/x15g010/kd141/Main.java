91cc7234622976f67b2c360fe7f60c21eab750e8
unknown <x15g010@PC504N014.chiba-fjb.ac.jp>
2016-01-18 15:03:20 +0900

    課題29

--- src/jp/ac/chibafjb/x15g010/kd141/Main.java
+++ src/jp/ac/chibafjb/x15g010/kd141/Main.java
@@ -1,9 +1,16 @@
 package jp.ac.chibafjb.x15g010.kd141;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sin =new Scanner(System.in);
+		System.out.print("整数を入力してください。--->");
+		int count = sin.nextInt();
+		sin.close();
 		
+		System.out.println("あなたの入力した数は"+count+"です。");
 
 	}
 
